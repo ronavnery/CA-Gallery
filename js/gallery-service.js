9abcfd97fd9eaea5313e515003e75daeb360c491
Ron Avnery <avnery@gmail.com>
2019-05-29 08:28:55 +0300

    Change date of projects to creation date

--- js/gallery-service.js
+++ js/gallery-service.js
@@ -6,34 +6,34 @@ var gProjects = createProjects();
 function createProjects() {
     return [
         createProject('Login Page', 'Simplicity is key', '',
-        'https://ronavnery.github.io/CA-SafeContent/', 'JS, jQuery, FlexBox'),
+        'https://ronavnery.github.io/CA-SafeContent/','May 26th, 2019', 'JS, jQuery, FlexBox'),
          
         createProject('Mine Sweeper', 'The good old game got a face lift', '',
-         'https://ronavnery.github.io/CA-minesweeper/', 'Recursive JS, Matrix, CSS'),
+         'https://ronavnery.github.io/CA-minesweeper/','May 19th, 2019', 'Recursive JS, Matrix, CSS'),
          
         createProject('Todos', 'Get things done', '',
-         'https://ronavnery.github.io/CA-todos/', 'JS, CSS, FlexBox'),
+         'https://ronavnery.github.io/CA-todos/','May 24th, 2019', 'JS, CSS, FlexBox'),
 
          createProject('Unusual Cart', 'How a cart will look in 50yrs time', '',
-         'https://ronavnery.github.io/CA-FutureCart/', 'JS, CSS, FlexBox'),
+         'https://ronavnery.github.io/CA-FutureCart/','May 21st, 2019', 'JS, CSS, FlexBox'),
 
          createProject('Guess Me', 'Self learning magician', '',
-         'https://ronavnery.github.io/CA-GuessMe/', 'JS, jQuery, CSS, FlexBox'),
+         'https://ronavnery.github.io/CA-GuessMe/','May 26th, 2019', 'JS, jQuery, CSS, FlexBox'),
 
          createProject('Book Store', 'Admin panel for book store managers', '',
-         'https://ronavnery.github.io/book-shop/', 'JS, jQuery, Bootstrap, FlexBox')
+         'https://ronavnery.github.io/book-shop/','May 27th, 2019', 'JS, jQuery, Bootstrap, FlexBox')
 
         ]
 }
 
-function createProject(name, title, desc, url, labels) {
+function createProject(name, title, desc, url, date, labels) {
     return {
         id: ++gNextId,
         name: name,
         title: title,
         desc: desc,
         url: url,
-        publishedAt: getTime(),
+        publishedAt: date,
         labels: labels
     }
 }
